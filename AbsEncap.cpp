cdb37070c04675b2efe13294065152ae1b159f02
saivenkatadada <adadasaivenkat0109@gmail.com>
2024-03-07 11:02:23 +0530

    Add files via upload

---
+++ AbsEncap.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class AccessSpecifierDemo
+{
+    private: int priVar;
+    protected: int proVar;
+    public: int pubVar;
+    public: void setVar(int priValue,int proValue, int pubValue)
+    {
+        priVar=priValue;
+        proVar=proValue;
+        pubVar=pubValue;
+    }
+    public: void getVar()
+    {
+        cout<<"Private, Protected and Public variables are "<<priVar<<", "<<proVar<<" and "<<pubVar<<endl;
+    }
+};
+int main()
+{
+    AccessSpecifierDemo obj;
+    int priValue,proValue,pubValue;
+    cout<<"Enter private, protected and public variables values (each in a separate line): "<<endl;
+    cin>>priValue>>proValue>>pubValue;
+    obj.setVar(priValue,proValue,pubValue);
+    obj.getVar();
+}
